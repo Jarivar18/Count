81e528c9637ce5172b9e4068cfdca16f1daf1445
Raviraj Dhakad <cs1170367@iitd.ac.in>
2017-12-14 21:55:09 +0530

    final commit

--- app/src/main/java/com/count/ravirajsinghdhakad/count/MainActivity.java
+++ app/src/main/java/com/count/ravirajsinghdhakad/count/MainActivity.java
@@ -2,12 +2,42 @@ package com.count.ravirajsinghdhakad.count;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView =(TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                String s=String.valueOf(count);
+                firstTextView.setText(s);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count=0;
+                String s=String.valueOf(count);
+                firstTextView.setText(s);
+            }
+        });
+
     }
 }
